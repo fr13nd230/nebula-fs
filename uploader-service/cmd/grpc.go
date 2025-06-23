4fad902e91900d78b516c79224d12a8a85b2fc00
Abderrahmane KADDOUR <darkvally4@gmail.com>
2025-06-23 20:54:31 +0100

    NFS [Uploader] | config(grpc): Implement gRPC Server starting with gracefull shutdown.

--- uploader-service/cmd/grpc.go
+++ uploader-service/cmd/grpc.go
@@ -1,2 +1,29 @@
 package main
 
+import (
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type gRPCServer struct {
+	port string
+}
+
+func NewConfig(port string) *gRPCServer {
+	return &gRPCServer{
+		port: port,
+	}
+}
+
+func (g *gRPCServer) NewGrpcServer() *grpc.Server {
+	return grpc.NewServer()
+}
+
+func (g *gRPCServer) Start(grpcServer *grpc.Server) error {
+	lis, err := net.Listen("tcp", g.port)
+	if err != nil {
+		return err
+	}
+	return grpcServer.Serve(lis)
+}
--- uploader-service/cmd/main.go
+++ uploader-service/cmd/main.go
@@ -10,32 +10,50 @@ import (
 	"github.com/fr13nd230/nebula-fs/uploader-service/config"
 )
 
-// main Entry point for any configuration mouting, healthcheck and the
-// point where we handle recovery and gracefull shutdowns.
 func main() {
-    ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
-    defer stop()
-    defer func() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	defer func() {
 		if r := recover(); r != nil {
 			slog.Info("[UploaderServer]: Recovering application from panic.", "original panic", r)
 		}
 	}()
-    
-    // serverErrs := make(chan error, 1)
 
 	err := config.InitConfig()
 	if err != nil {
 		slog.Error("[UploaderServer]: Failed to mount config files.", "error", err)
+		return
 	}
-	
+
+	gRPCPort := config.GetVariable("GRPC_PORT")
+	if gRPCPort == "" {
+		slog.Error("[UploaderServer]: GRPC_PORT is not set in the config")
+		return
+	}
+
+	grpcErrs := make(chan error, 1)
+	cfg := NewConfig(gRPCPort)
+	grpcServer := cfg.NewGrpcServer()
+
+	go func() {
+		grpcErrs <- cfg.Start(grpcServer)
+	}()
+
+	slog.Info("[UploaderServer]: gRPC Server has been succesfully launched.", "port", cfg.port)
+
 	select {
 	case <-ctx.Done():
-	    slog.Error("[UploaderServer]: Gracefull shutdown has been scheduled, shutdown signal received ....")
-		_, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-        defer cancel()
-        
-        // Use shutCtx to shut down any operations in here ... 
-        
-        slog.Info("[UploaderServer]: Uploader server has been shutdowned succesfully.")
+		slog.Error("[UploaderServer]: Gracefull shutdown has been scheduled, shutdown signal received ....")
+		_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		grpcServer.GracefulStop()
+		slog.Info("[UploaderServer]: Uploader server has been shutdowned succesfully.")
+
+	case err := <-grpcErrs:
+		slog.Error("[UploaderServer]: Failed to start new gRPC server.", "error", err)
+		slog.Info("[UploaderServer]: Starting to gracefully shutdown the gRPC server ....")
+		grpcServer.GracefulStop()
+		slog.Info("[UploaderServer]: gRPC server has been gracefully shutdowned.")
 	}
 }
